96dab8aa9a6bab4c5755c4c6fa80da1b9bd5923c
Cairo Mateus <cairomnr@hotmail.com>
2018-12-10 08:20:23 -0200

    Atualizando código do somador/subtrator completo.

--- somador_subtrator_completo/main.c
+++ somador_subtrator_completo/main.c
@@ -22,8 +22,8 @@
 #define LCD_D7 3
 
 // Declaração das funções.
-bool getSaida(boolean, boolean, boolean, boolean);
-bool getTrasposicaoSaida(boolean, boolean, boolean, boolean);
+bool getSaida(bool, bool, bool, bool);
+bool getTrasposicaoSaida(bool, bool, bool, bool);
 
 LiquidCrystal lcd(
   LCD_RS,
@@ -38,9 +38,7 @@ LiquidCrystal lcd(
  * Define as configurações das portas.
  */
 void setup()
-{
-   	Serial.begin(9600);
-  
+{  
   	// DIP 1
   	pinMode(DIP1_1, INPUT);
   	pinMode(DIP1_2, INPUT);
@@ -58,6 +56,8 @@ void setup()
   
   	// LCD
     lcd.begin(16, 2);
+  	lcd.print("SOM/SUB COMPLETO");
+  	delay(10);
 }
 
 /**
@@ -65,11 +65,11 @@ void setup()
  */
 void loop()
 {
-  int i, entradaA, entradaB, resposta;
-  boolean transpSaida = 0;
+  int i, entradaA, entradaB, resposta = 0;
+  bool transpSaida = 0;
   byte DIP1Portas[] = {DIP1_1, DIP1_2, DIP1_3, DIP1_4};
   byte DIP2Portas[] = {DIP2_1, DIP2_2, DIP2_3, DIP2_4};
-  boolean DIP1[NU_BITS], DIP2[NU_BITS], M, RESP[NU_BITS + 1];
+  bool DIP1[NU_BITS], DIP2[NU_BITS], M, RESP[NU_BITS + 1];
 
   M = digitalRead(DIP_M);
   
@@ -78,33 +78,43 @@ void loop()
   	DIP2[i] = digitalRead(DIP2Portas[i]);
   }
   
-  for (i = NU_BITS - 1; i > -1; i++) {
-  	RESP[i + 1] = getSaida(M, DIP1[i], DIP2[i], transpSaida);
-    transpSaida = getTrasposicaoSaida(M, DIP1[i], DIP2[i], transpSaida);
+  for (i = 0; i < NU_BITS; i++) {
+   	int pos = NU_BITS - i - 1;
+  	RESP[i] = getSaida(M, DIP1[pos], DIP2[pos], transpSaida);
+    transpSaida = getTrasposicaoSaida(M, DIP1[pos], DIP2[pos], transpSaida);
   }
  
-  RESP[0] = getSaida(M, 0, 0, transpSaida);
+  RESP[4] = getSaida(M, 0, 0, transpSaida);
  
   entradaA = converteBoolParaInteiro(DIP1);
   entradaB = converteBoolParaInteiro(DIP2);
   
-  if (RESP[0]) resposta += 16;
-  if (RESP[1]) resposta += 8;
+  if (RESP[4]) resposta += 16;
+  if (RESP[3]) resposta += 8;
   if (RESP[2]) resposta += 4;
-  if (RESP[3]) resposta += 2;
-  if (RESP[4]) resposta += 1;
+  if (RESP[1]) resposta += 2;
+  if (RESP[0]) resposta += 1;
   
   lcd.clear();
-  lcd.display();
   lcd.setCursor(1, 0);
   lcd.print(entradaA);
-  lcd.print(" AVBB ");
+  
+  if (M) {
+    lcd.print(" - ");
+    
+    if(entradaA < entradaB) {
+    	resposta -= 32;
+    }
+  } else {
+    lcd.print(" + ");
+  }
+  
   lcd.print(entradaB);
   
   lcd.setCursor(1, 1);
   lcd.print(resposta);
   
-  delay(10);
+  delay(1000);
 }
 
 /**
@@ -113,7 +123,7 @@ void loop()
  * @params boolean M, A, B, Te
  * @return boolean
  */
-bool getSaida(boolean M, boolean A, boolean B, boolean Te) {
+bool getSaida(bool M, bool A, bool B, bool Te) {
 	return (A && !B && !Te)
       || (!A && !B && Te)
       || (A && B && Te)
@@ -126,7 +136,7 @@ bool getSaida(boolean M, boolean A, boolean B, boolean Te) {
  * @params boolean M, A, B, Te
  * @return boolean
  */
-bool getTrasposicaoSaida(boolean M, boolean A, boolean B, boolean Te) {
+bool getTrasposicaoSaida(bool M, bool A, bool B, bool Te) {
   return (B && Te)
     || (!M && A && B)
     || (!M && A && Te)
@@ -134,8 +144,13 @@ bool getTrasposicaoSaida(boolean M, boolean A, boolean B, boolean Te) {
     || (M && !A && Te);
 }
 
-
-int converteBoolParaInteiro(boolean *valores) {
+/**
+ * Converte de valores booleanos para um valor inteiro.
+ *
+ * @param bool *valores
+ * @return int
+ */
+int converteBoolParaInteiro(bool *valores) {
   int resp = 0;
   
   if (valores[0]) resp += 8;
@@ -144,5 +159,4 @@ int converteBoolParaInteiro(boolean *valores) {
   if (valores[3]) resp += 1;
   
   return resp;
-
 }
